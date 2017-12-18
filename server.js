d0eb25e9b9b90c8a7df96aaa1c60d27b40f9ddba
Nitish Bansal <nitishbansal2297@gmail.com>
2017-12-18 20:44:03 +0530

    Added Ball prototype

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ app.use(express.static("public"))
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
 
+var health = 100;
+var WIDTH = 900;
+var HEIGHT = 900;
+var firstBall = 1;
+var firstTank = 1;
+
 app.listen(4000, function(){
 	console.log("Server started at port 4000" + __dirname);
 })
@@ -15,7 +21,6 @@ app.get("/", function(req, res){
 function GameArena(){
 	this.tanks = [];
 	this.balls = [];
-
 }
 
 GameArena.prototype.addtank = function(tank){
@@ -49,10 +54,11 @@ GameArena.prototype.syncBall = function(){
 		// if there is collision 
 		this.detectCollison(t);
 
-		if(t.x <= 0 || t.x >= 40 || t.y <= 0 || t.y >= 500)
+		if(t.x <= 0 || t.x >= WIDTH || t.y <= 0 || t.y >= HEIGHT)
 			t.dead = true;
 	}
 }
+
 GameArena.prototype.detectCollison = function(ball){
 
 	this.tanks.forEach(function(t){
@@ -60,10 +66,13 @@ GameArena.prototype.detectCollison = function(ball){
 		{
 			ball.dead = true;
 		}
+		else
+			ball.keepMoving();
 	})
 }
 
 GameArena.prototype.removeDeadTank = function(){
+	
 	this.tanks.forEach(function(t){
 		if(t.health <= 0)
 		{
@@ -73,6 +82,7 @@ GameArena.prototype.removeDeadTank = function(){
 }
 
 GameArena.prototype.removeDeadBall = function(){
+	
 	this.balls.forEach(function(t){
 		return !t.dead;
 	})
@@ -86,7 +96,7 @@ io.on("connection", function(){
 
 	var startX = randomInteger(40,500);
 	var startY = randomInteger(40,500);
-	var tankID = guid();
+	var tankID = getTankID();
 
 	socket.emit("addtank", {id: tankID, name: tank.name, islocal: true, type: tank.type, x: startX, y: startY, health: IntialHealth})
 	socket.broadcast.emit("addtank", {id: tankID, name: tank.name, islocal: false, type: tank.type, x: startX, y: startY, health: IntialHealth})
@@ -105,8 +115,28 @@ io.on("connection", function(){
 		socket.broadcast.emit("sync", {tanks : game.tanks, balls : game.balls});
 
 	})
+
+	socket.on("shoot",function(ball)){
+		var ball = new Ball(ball.angle, ball.x, ball.y);
+		game.addball(ball);
+	}
+
 })
 
+function Ball(angle, x, y){
+	this.id = getBallID();
+	this.angle = angle;
+	this.x = x;
+	this.y = y;
+	this.dead = false;
+
+}
+
+Ball.prototype.keepMoving = function(){
+
+	this.x = this.x + ballSpeed*Math.cos(this.angle);
+	this.y = this.y + ballSpeed*Math.sin(this.angle);
+}
 
 function randomInteger(min, max){
 	return Math.floor(Math.random() * (max - min)) + min;
