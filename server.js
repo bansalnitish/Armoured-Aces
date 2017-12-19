1fa6417d3091d47bd7e862245bb173056348a4e8
Nitish Bansal <nitishbansal2297@gmail.com>
2017-12-19 22:48:45 +0530

    Removed Errors

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var firstBall = 1;
 var firstTank = 1;
 
 app.listen(4000, function(){
-	console.log("Server started at port 4000" + __dirname);
+	console.log("Server started at port 4000");
 })
 
 app.get("/", function(req, res){
@@ -50,13 +50,13 @@ GameArena.prototype.synctank = function(tank){
 }
 // balls are handled by server only so no data is recieved from the client except the shoot
 GameArena.prototype.syncBall = function(){
-	this.balls.forEach(function(t)){
+	this.balls.forEach(function(t){
 		// if there is collision 
 		this.detectCollison(t);
 
 		if(t.x <= 0 || t.x >= WIDTH || t.y <= 0 || t.y >= HEIGHT)
 			t.dead = true;
-	}
+	})
 }
 
 GameArena.prototype.detectCollison = function(ball){
@@ -92,18 +92,18 @@ var game = new GameArena();
 
 io.on("connection", function(){
 	console.log("user connected");
-	socket.on("new-tank", function(tank){
+	socket.on("newTank", function(tank){
 
 	var startX = randomInteger(40,500);
 	var startY = randomInteger(40,500);
 	var tankID = getTankID();
 
-	socket.emit("addtank", {id: tankID, name: tank.name, islocal: true, type: tank.type, x: startX, y: startY, health: IntialHealth})
-	socket.broadcast.emit("addtank", {id: tankID, name: tank.name, islocal: false, type: tank.type, x: startX, y: startY, health: IntialHealth})
+	socket.emit("addTank", {id: tankID, name: tank.name, islocal: true, type: tank.type, x: startX, y: startY, health: IntialHealth})
+	socket.broadcast.emit("addTank", {id: tankID, name: tank.name, islocal: false, type: tank.type, x: startX, y: startY, health: IntialHealth})
 
 	game.addtank({id: tankID, name: tank.name, type: tank.type});
 
-	}
+	})
 
 	socket.on("sync", function(tank){
 		game.synctank(tank);
@@ -116,10 +116,10 @@ io.on("connection", function(){
 
 	})
 
-	socket.on("shoot",function(ball)){
+	socket.on("shoot",function(ball){
 		var ball = new Ball(ball.angle, ball.x, ball.y);
 		game.addball(ball);
-	}
+	})
 
 })
 
@@ -136,6 +136,22 @@ Ball.prototype.keepMoving = function(){
 
 	this.x = this.x + ballSpeed*Math.cos(this.angle);
 	this.y = this.y + ballSpeed*Math.sin(this.angle);
+}
+
+function getTankID(){
+	
+	if(firstTank == 1000)
+		firstTank = 0;
+
+	return firstTank++;
+}
+
+function getBallID(){
+	
+	if(firstBall == 1000)
+		firstBall = 0;
+
+	return firstBall++;
 }
 
 function randomInteger(min, max){
