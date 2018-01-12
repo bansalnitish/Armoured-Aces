0a27be57595d648c1cfdbeef2a2c125f87261608
Nitish Bansal <nitishbansal2297@gmail.com>
2018-01-13 00:53:30 +0530

    fixx

--- public/js/socket.io.js
+++ public/js/socket.io.js
@@ -1,21 +1,3 @@
-Skip to content
-This repository
-Search
-Pull requests
-Issues
-Marketplace
-Explore
- @bansalnitish
- Sign out
- Watch 8
-  Star 68  Fork 87 rubentd/tanks
- Code  Issues 5  Pull requests 0  Projects 0  Wiki  Insights
-Branch: master Find file Copy pathtanks/www/js/socket.io.js
-9a633ed  on 9 Jan 2015
-@rubentd rubentd Add socket connections to play multiplayer
-1 contributor
-RawBlameHistory    
-6714 lines (5668 sloc)  163 KB
 !function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.io=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
 
 module.exports = _dereq_('./lib/');
